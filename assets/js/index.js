5250485559031d0190c69fc157c6f5a3d0d2e3bd
itsukichan <kou05ituki09design@gmail.com>
2025-03-26 22:17:20 +0900

    add section block

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,5 +4,10 @@ app.innerHTML = `
   <div class="sample-code">
     <h1>Hello World</h1>
     <p>lorem  ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptates.</p>
+
+    <section>
+      <h2>Sub Title</h2>
+      <p>lorem  ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, volupt
+    </section>
   </div>
 `;
